cb311232898f7c746ee6a7b1e4fd28f5bb934a26
Abhinav Kannojia <abhinav.kannojia1@gmail.com>
2019-06-13 14:40:56 -0700

    first commit

--- Word_count_hash.rb
+++ Word_count_hash.rb
@@ -1,1 +1,18 @@
-def Word_counthash()
+class Solution
+  def Word_counthash(text)
+    arr = []
+    while text.length > 0 do
+      arr << text[0, 1]
+      text[0, 1] = ""
+    end
+
+    c = Hash.new(0)
+    arr.each do |arr|
+      c[arr] += 1
+    end
+    print c
+  end
+end
+
+
+Solution.new.Word_counthash("Hello World")
