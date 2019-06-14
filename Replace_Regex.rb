7aa63223a948fd97107402d022ed7c7b4b45436c
Abhinav Kannojia <abhinav.kannojia1@gmail.com>
2019-06-14 11:41:05 -0700

    Replace_Regex

--- Replace_Regex.rb
+++ Replace_Regex.rb
@@ -1,9 +1,21 @@
-class Solution
-  def Replace_Regex(text)
-    text.gsub!(/[aeiou]/,"*")
-    print text
+class Replace
+  def replace_regex(text)
+    if text == " "
+      return "Please provide an input"
+    else
+      text.gsub!(/[aeiou]/,"*")
+      return text
+    end
   end
 end
 
-
-Solution.new.Replace_Regex("abhinav")
+puts "Enter input"
+text = gets.chomp
+a = Replace.new.replace_Regex(" ")
+print a
+b = Replace.new.replace_Regex("password123")
+print b
+c = Replace.new.replace_Regex("good morning")
+print c
+d = Replace.new.replace_Regex("hello everyone")
+print d
