d39a921cff5a8aaf523c495ff3d29eec42a0c6b0
Abhinav Kannojia <abhinav.kannojia1@gmail.com>
2019-06-24 11:51:34 -0700

    Facotrial_updated1

---
+++ Factorial_updated1.rb
@@ -0,0 +1,14 @@
+# Factorial
+class Factorial
+  def factorial(number)
+    return puts 0 if number == 0
+    number = number.to_i
+    number_range = number.downto(1).to_a
+    factorial = number_range.inject(:*)
+    puts "The factorial of #{number} is #{factorial}"
+  end
+end
+
+a = Factorial.new
+number = ARGV[0].to_i
+a.factorial(number)
